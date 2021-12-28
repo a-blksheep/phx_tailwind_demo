f241f485c56d23f9dce47cbce2157943c4eed4a9
Jason Lamb <blksheep@tutanota.com>
2021-12-28 11:02:49 +0200

    Added Tailwind configuration

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,8 @@ config :phx_tailwind_demo, TailwindDemoWeb.Endpoint,
   secret_key_base: "G+/pLOJawFBoZVClZ+6nHf12ykVvlCdJzlcTNPECtYt2QdIvRS5JkOlnL1p6Et8y",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
